af1679219137f55750b87e0b6c592ffa1bf38187
Your Name <renee.petit@gmail.com>
2021-09-12 16:57:35 -0700

    check for required

--- script.js
+++ script.js
@@ -18,13 +18,46 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+// check email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+
+    checkRequired([username, email, password, password2])
     
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+    // if(username.value === '') {
+    //     showError(username, 'Username is required');
+    // } else {
+    //     showSuccess(username);
+    // }
+
+    // if(email.value === '') {
+    //     showError(email, 'Email is required');
+    // } else if(!isValidEmail(email.value)) {
+    //     showError(email, "Email is not valid");
+    // }
+    // else {
+    //     showSuccess(email);
+    // }
 })
